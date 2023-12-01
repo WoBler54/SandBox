dc0f209ad542b8b154f0ca84b9f6bd54689e207d
Егор Трафимов <19rabigu@gmail.com>
2023-12-01 21:32:07 +0100

    SumArray

---
+++ 8kyu/sum-arrays.cs
@@ -0,0 +1,22 @@
+/* DESCRIPTION:
+Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+Examples
+Input: [1, 5.2, 4, 0, -1]
+Output: 9.2
+
+Input: []
+Output: 0
+
+Input: [-2.398]
+Output: -2.398 
+SOLUTIONS */
+using System.Linq;
+
+public class Kata
+{
+  public static double SumArray(double[] array)
+  {
+    return array.Sum();
+  }
+}
