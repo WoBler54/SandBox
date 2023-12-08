92ee53c51ad800f41559aa00985766fc79d6c815
Trafimau Egor <71026019+WoBler54@users.noreply.github.com>
2023-12-08 20:29:15 +0100

    refactoring

--- 8kyu/basic-mathematical-operations.js
+++ 8kyu/basic-mathematical-operations.js
@@ -9,18 +9,34 @@
 
 function basicOp(operation, value1, value2)
 {
-  if(operation == '+'){
-    return value1 + value2;
-  }else{
-    if(operation == '-'){
+   // if(operation == '+'){
+  //   return value1 + value2;
+  // }else{
+  //   if(operation == '-'){
+  //     return value1 - value2;
+  //   }else{
+  //     if (operation == '*'){
+  //       return value1 * value2;
+  //     }else {
+  //       return value1 / value2;
+  //     }
+  //   }
+  // }
+
+
+  switch(operation){
+    
+    case '+':
+      return value1 + value2;
+
+    case '-':
       return value1 - value2;
-    }else{
-      if (operation == '*'){
-        return value1 * value2;
-      }else {
-        return value1 / value2;
-      }
-    }
-  }
+
+    case '*':
+      return value1 * value2;
+
+    default:
+      return value1 / value2;
+
   
 }
