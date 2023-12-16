276000025b85f9edb86141b62d63a79c0eac8a0d
Yahor Trafimau <19rabigu@gmail.com>
2023-12-16 18:01:46 +0100

    another-kata

---
+++ 8kyu/the-wide-mouth-frog.js
@@ -0,0 +1,12 @@
+// The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+// He just can't stop asking the creatures he encounters what they like to eat. 
+// But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+// When he meets the alligator, it then makes a tiny mouth.
+
+function mouthSize(animal) {
+    if(animal.toLowerCase() == 'alligator' ){
+      return 'small';
+    }else return 'wide';
+  }
