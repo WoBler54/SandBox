6ed45e79b8bd0085cef00e3217b0700d2b25dac7
Yahor Trafimau <19rabigu@gmail.com>
2023-12-19 23:34:19 +0100

    another-kata

---
+++ 8kyu/set-alarm.js
@@ -0,0 +1,9 @@
+// The function should return true if you are employed and not on vacation 
+// (because these are the circumstances under which you need to set an alarm).
+//  It should return false otherwise. Examples:
+
+function setAlarm(employed, vacation){
+    if(employed && !vacation){
+      return true;
+    }else return false;
+  }
