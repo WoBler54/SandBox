767368ec931b75d3fa7e2091aad5361491979b9f
Yahor Trafimau <19rabigu@gmail.com>
2023-12-19 23:24:00 +0100

    another-kata

---
+++ 8kyu/sentence-smash.js
@@ -0,0 +1,14 @@
+// Sentence SmashWrite a function that takes an array of words and smashes 
+// them together into a sentence and returns the sentence. You can ignore 
+// any need to sanitize words or add punctuation, but you should add spaces 
+// between each word. Be careful, there shouldn't be a space at the beginning 
+// or the end of the sentenc
+
+function smash (words) {
+    let answer = '';
+    for( let word of words){
+      answer += word + ' ';
+      
+    }
+    return answer.trim();
+  };
