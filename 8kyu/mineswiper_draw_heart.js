1887c4f927cc4bfda670da92939229a0f78b83c2
Yahor Trafimau <19rabigu@gmail.com>
2023-12-19 23:24:28 +0100

    personal-project.

---
+++ 8kyu/mineswiper_draw_heart.js
@@ -0,0 +1,15 @@
+// need drawe a hart on the mineswiper field
+
+
+function drawHart(arrI,arrJ){
+    widhtField = 20;
+    hightField = 20;
+    for(let i = 0; i <= widhtField; i++){
+        document.write(i);
+        for(let j = 0; j <= hightField; j++){
+            document.write(j);
+        }
+    }
+
+    
+}
