d4949e4031c63374883e9248074121de50023ad2
Yahor Trafimau <19rabigu@gmail.com>
2023-12-23 15:27:59 +0100

    i`m-do-it

--- 8kyu/mineswiper_draw_heart.js
+++ 8kyu/mineswiper_draw_heart.js
@@ -1,7 +1,7 @@
 // need drawe a hart on the mineswiper field
 
 
-function drawHart(strI,strJ){
+function drawHeart(strI,strJ){
 
     var arrIStr = strI.split(' ');
     var arrJStr = strJ.split(' ');
@@ -28,24 +28,15 @@ for ( let i = 0; i < arrI.length; i++ ){
         
     } 
     arrayOfArrays.push(transportArray);
-// for (let i = 0; i < hightField; i++){
-//     if(true){
-//         document.write('*')
-//     }else {
-//         document.write(0);
-//     }
-//     for(let j = 0; j < hightField; j++){
 
-//     }
-// }
     
-const rows = 15;
-const columns = 15;
+const rows = 19;
+const columns = 19;
 
-// Создаем пустой массив массивов
+
 const array2D = [];
 
-// Заполняем массив массивов нулями
+
 for (let i = 0; i < rows; i++) {
   const row = [];
   for (let j = 0; j < columns; j++) {
@@ -55,17 +46,35 @@ for (let i = 0; i < rows; i++) {
 }
 
 for(let i = 0; i < arrayOfArrays.length; i ++){
-    for( let j = 0; j < arrayOfArrays[i].length; j++){
+    for( let j = 0; j < arrayOfArrays[i].length ; j++){
         array2D[i][arrayOfArrays[i][j]] = '*'
     }
 }
+
+array2D[0][0] = 0;
+array2D[1][0] = 0;
+array2D[2][0] = 0;
+    
+
+for(let i = 0; i < array2D.length; i++){
+    for(let j = 0; j < array2D.length; j++){
+        if(array2D[i][j] == '*'){
+           if(array2D[i][j - 1] != '*') array2D[i][j - 1] += 1;
+           if(array2D[i + 1][j] != '*') array2D[i + 1][j] += 1;
+           if(array2D[i - 1][j] != '*') array2D[i - 1][j] += 1;
+            if(array2D[i][j + 1] != '*')array2D[i][j + 1] += 1;
+            if(array2D[i - 1][j + 1] != '*') array2D[i - 1][j + 1] += 1;
+            if(array2D[i + 1][j + 1] != '*') array2D[i + 1][j + 1] += 1;
+            if(array2D[i - 1][j - 1] != '*') array2D[i - 1][j - 1] += 1;
+            if(array2D[i + 1][j - 1] != '*') array2D[i + 1][j - 1] += 1;
+         
+        }
+    }
+}
+        
 for (let i = 0; i < rows; i++) {
     document.write(array2D[i].join(" ") + "<br>");
-    }        
-
-
-        
-
+    }    
 
 
 }
