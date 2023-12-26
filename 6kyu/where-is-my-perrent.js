2dd24cc2781f7c1ad02666b74552542f29c8dc52
Yahor Trafimau <19rabigu@gmail.com>
2023-12-26 06:16:22 +0100

    another-kata

---
+++ 6kyu/where-is-my-perrent.js
@@ -0,0 +1,31 @@
+// DESCRIPTION:
+// Mothers arranged a dance party for the children in school. At that party, there are only mothers and their children.
+//  All are having great fun on the dance floor when suddenly all the lights went out. 
+//  It's a dark night and no one can see each other. But you were flying nearby and you can see in the dark 
+//  and have ability to teleport people anywhere you want.
+
+// Legend:
+// -Uppercase letters stands for mothers, lowercase stand for their children, i.e. "A" mother's children are "aaaa".
+// -Function input: String contains only letters, uppercase letters are unique.
+// Task:
+// Place all people in alphabetical order where Mothers are followed by their children, i.e. "aAbaBb" => "AaaBbb".
+
+
+function findChildren(dancingBrigade) {
+    //	console.log(dancingBrigade.sort());
+    
+       let arrWithDancing = dancingBrigade.toLowerCase().split('').sort();
+       let result = '';
+       result += arrWithDancing[0].toUpperCase();
+       for(let i = 1; i < arrWithDancing.length; i++){
+             if(arrWithDancing[i] != arrWithDancing[i+1] && arrWithDancing[i+1] != undefined){
+                result += arrWithDancing[i];
+                result += arrWithDancing[i + 1].toUpperCase();
+                i++;
+             }else {
+                result += arrWithDancing[i];
+             }
+       }
+       return result;
+    }
+    
