8b706d3573cc70911f86dc7e61eae4d6fea1b669
Yahor Trafimau <19rabigu@gmail.com>
2023-12-27 10:32:50 +0100

    another-kata

---
+++ 8kyu/beginer-arr.js
@@ -0,0 +1,11 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+function maps(x){
+    let result = [];
+     for(let num of x){
+       result.push(num * 2);
+     }
+     return result;
+   }
