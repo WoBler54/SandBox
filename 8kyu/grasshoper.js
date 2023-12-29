de1724230af801ce80dc451cb2439e2a870a99b1
Yahor Trafimau <19rabigu@gmail.com>
2023-12-29 17:35:52 +0100

    another-kata

---
+++ 8kyu/grasshoper.js
@@ -0,0 +1,6 @@
+// Create a function that gives a personalized greeting. 
+// This function takes two parameters: name and owner.
+
+function greet (name, owner) {
+    return name != owner ? 'Hello guest' : 'Hello boss';
+  }
