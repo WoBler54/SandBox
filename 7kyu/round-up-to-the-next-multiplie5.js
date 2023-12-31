1b878fd92d08dc1222c98933be3e2683fe687fe9
Yahor Trafimau <19rabigu@gmail.com>
2023-12-31 21:50:03 +0100

    another-kata

---
+++ 7kyu/round-up-to-the-next-multiplie5.js
@@ -0,0 +1,9 @@
+
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+function roundToNext5(n){
+    for(n ; n % 5 != 0; n++){
+      
+    }
+    return n;
+  }
