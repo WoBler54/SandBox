51df9d1e954b2bb5bcecb0fa6774d49ae1182b42
Yahor Trafimau <19rabigu@gmail.com>
2023-12-31 21:50:35 +0100

    another-kata

---
+++ 8kyu/traning.js
@@ -0,0 +1,5 @@
+//n JavaScript, your code is running in a function, let us step by step complete your first JS function.
+helloWorld = function() {
+    var str = "Hello World!";
+    console.log( str );
+  }
