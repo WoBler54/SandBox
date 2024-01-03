f77aa4102f91faada2652a3cdfc0443af3c72b76
Yahor Trafimau <19rabigu@gmail.com>
2024-01-03 19:23:06 +0100

    another-kata

---
+++ 5kyu/numbers-that-are-a-power-of-their-sum-of-digits.js
@@ -0,0 +1,19 @@
+// testing(powerSumDigTerm(1), 81);
+// testing(powerSumDigTerm(2), 512);
+// testing(powerSumDigTerm(3), 2401);
+// testing(powerSumDigTerm(4), 4913);
+// The number 81 has a special property, a certain power of the sum of its digits is equal to 81 (nine squared). 
+// Eighty one (81), is the first number in having this property (not considering numbers of one digit). 
+// The next one, is 512. Let's see both cases with the details
+
+// 8 + 1 = 9 and 92 = 81
+
+// 512 = 5 + 1 + 2 = 8 and 83 = 512
+
+// We need to make a function that receives a number as argument n and returns the n-th term of this sequence of numbers.
+
+
+function powerSumDigTerm(n) {
+
+ 
+}
